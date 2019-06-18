3db278358e01924d11ba1085ca8920eb5db8eeee
Pierre Legay <Pierre.Legay@recia.fr>
2019-06-18 09:34:39 +0200

    Taille > Breakpoint => max display = 5

--- src/services/breakpointService.js
+++ src/services/breakpointService.js
@@ -2,13 +2,22 @@ function nbViewByBreakpoint(glideBreakpoints) {
     //  glideBreakpoints = this.glideOptions['breakpoints']
     var width = window.innerWidth;
     var nbView = 0;
-    glideBreakpoints.every(function(item, index) {
-      if (width <= index ) {
-        nbView = item.perView;
-        return false;
-      } 
-      return true;
-    });
+    // eslint-disable-next-line
+    console.log('nbViewByBreackpoint width =' +  width);
+    if (glideBreakpoints && glideBreakpoints.every) { 
+      nbView = 5;
+      glideBreakpoints.every(function(item, index) {
+        // on cherche le break point correspondant a la taille 
+        // eslint-disable-next-line
+        console.log('breakpoint (item, index)=(' +  item.perView + ", "+ index + ')');
+        if (width <= index ) {
+
+          nbView = item.perView;
+          return false;
+        } 
+        return true;
+      });
+    }
     return ~~(nbView);
 }
 
@@ -16,7 +25,7 @@ function initBreakpoints (nbJour) {
     // nbJour = this.nbJour; glideOptions = this.glideOptions
     var largeur = parseInt(process.env.VUE_APP_BREAKPOINT_WIDTH)
     var winWidth = window.innerWidth;
-    var offsetWidth = document.querySelector('menu-cantine-menu-semaine').offsetWidth;
+    var offsetWidth = document.querySelector('div#menusemaine').offsetWidth;
     var delta = ~~(2 * (winWidth - offsetWidth)/ ((nbJour +1) * nbJour) )
   
     var breakpoints = [];
@@ -33,6 +42,8 @@ function initBreakpoints (nbJour) {
 function activeByBreakpoint(breakpoints, dateInit, nbJours, jours) {
     // dateInit = json.requete.dateJour, nbJours = json.nbJours, jours = json.jours
     var nbVisible = nbViewByBreakpoint(breakpoints);
+    // eslint-disable-next-line
+    console.log('nbVisible =' +  nbVisible);
     var len;
     var i;
     for (i = 0, len = nbJours; i < len; i++) {
@@ -41,13 +52,20 @@ function activeByBreakpoint(breakpoints, dateInit, nbJours, jours) {
         var pos = i + 1 - ~~((nbVisible + 1)/2);
         if (pos > 0 ) {
           if (pos + nbVisible < len) {
+            // eslint-disable-next-line
+            console.log('1 active =' + pos );
             return pos;
           } 
-          return len - nbVisible;
-        }
+          pos = len - nbVisible;
+          // eslint-disable-next-line
+          console.log('2 active =' + pos + " " + len + " " + nbVisible);
+          return pos
+        }// eslint-disable-next-line
+        console.log('3 active =0' + pos );
         return  0;
         
       }
     }
+    return 0;
 }
 export { nbViewByBreakpoint, initBreakpoints , activeByBreakpoint};
